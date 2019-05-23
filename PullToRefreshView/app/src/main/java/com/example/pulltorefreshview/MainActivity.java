558856852834638300499afd43f66c439ce4d0c6
hellodonj <664145960@qq.com>
2019-05-23 14:30:02 +0800

    ButterKnife的使用。

--- PullToRefreshView/app/src/main/java/com/example/pulltorefreshview/MainActivity.java
+++ PullToRefreshView/app/src/main/java/com/example/pulltorefreshview/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.pulltorefreshview;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 /**
  * 描述: 主功能
  * 作者|时间: djj on 2019/5/23 0023 上午 11:52
@@ -10,14 +13,14 @@ import android.support.v7.app.AppCompatActivity;
  */
 public class MainActivity extends AppCompatActivity {
 
-    private PullToRefreshView mRefreshView;
+    @BindView(R.id.refresh_view)
+    PullToRefreshView mRefreshView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        mRefreshView = findViewById(R.id.refresh_view);
+        ButterKnife.bind(this);
 
         mRefreshView.setOnHeaderRefreshListener(new PullToRefreshView.OnHeaderRefreshListener() {
             @Override
